795dd8f9890fe2f1be20162f87dc78fb56461876
Ramalheira07 <liriosnoop@hotmail.com>
2015-03-14 17:37:18 +0000

    Analisa este

--- projecto_iaed.c
+++ projecto_iaed.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+# define TAMANHO_NOME 40
+# define TAMANHO_MATRIZ_MAX 1000; 
 
 typedef struct{
 	int NumeroRegisto,Classificacao;
 	char Nome[TAMANHO_NOME];
 }Banco;
 
-Banco RegistarBanco(int numero,char nome,int classificacao);
+typedef struct{
+    Banco banco;
+    int Credito_UM;
+}Elemento;
+
+Elemento Matriz[TAMANHO_MATRIZ_MAX][TAMANHO_MATRIZ_MAX];
+int NumBancosExistentes=0;
+
+Banco RegistarBanco(int numero,char nome,int classificacao,Elemento Matriz[][],NumBancosExistentes);
 Banco AlterarClassificacao(Banco banco,int classificacao);
 int CompararBancos(Banco banco1,Banco banco2);
 int E_Banco(Banco banco);
 int VerClassificacao(Banco banco);
+
